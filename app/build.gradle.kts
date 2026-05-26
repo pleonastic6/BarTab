ac58b904835b201e3c57af4eb2c10676a06af9ff
Arturo Renner <Artur.renner6@gmail.com>
2026-05-26 12:21:10 +0200

    Add Material Components theme dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+    implementation("com.google.android.material:material:1.12.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
